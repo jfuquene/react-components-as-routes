b6f3f6de8aa7f509db4acd898a78fac2a893b538
John Fuquene <jfuquene@email.wm.edu>
2021-04-14 22:03:47 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './home'
+import About from './about'
+import Login from './login'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 
-const Home = () => {
-  return (
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'red',
+  textDecoration: 'none',
+  color: 'white',
+}
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/" exact style={link} activeStyle={{ background: 'darkblue'}}>Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
+
+ReactDOM.render((
+  <Router>
     <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+    <Navbar /> 
+    <Route path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/login" component={Login} />
+   </div>
+  </Router>),
   document.getElementById('root')
 );
